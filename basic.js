fb2fe6257e3206f7fc039e8a416ad449c1089cb0
prismtechbd3 <prismtechb@gmail.com>
2024-06-05 02:23:34 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,97 @@
+console.log("Hello World")
+// console.log(5+2)
+
+
+variable
+var a = 5;
+ a = 15;
+
+// console.log(a);
+
+// variable naming
+
+// valid name
+ var x = 5;
+ var age =50;
+ var my_name = "Shuvo";
+ var nam1 = "Das";
+ var $hlw ="Helllo Ami Shuvo"
+// console.log(x,age,my_name,nam1,$hlw);
+
+// invalid
+// var 1hello=5;
+// var my-age=25;
+// var if;
+
+
+// Data types
+
+var age= 25; //number
+var name= 'Shuvo'; //string
+var x = true; //boolean
+
+var pi= 3.1416;
+
+var x = parseInt(pi);
+// console.log((x));
+
+
+// var num = 50.7545464;
+// console.log(typeof (num));
+
+var floating = parseFloat(num);
+
+var integer = parseInt(floating);
+var fixed= floating.toFixed(3);
+
+// console.log(floating);
+// console.log(integer);
+// console.log(fixed);
+
+var x= 10;
+var y= 17;
+var z= x+y;
+
+// console.log(z);
+var x = x+5;
+// console.log (x);
+
+var x = 10;
+x= x+1
+x+=1;
+x++ //post increment
+++x //pre increment
+
+
+var x =5;
+// console.log(x++); //print hocce 5 then jog hocce 1
+// console.log (x++); //print hocce 6 then jog hocce 1
+
+
+var y = 10;
+
+// console.log(++y); //print hocce 10 then jog hocce 1
+// console.log(++y); //print hocce 11 then jog hocce 1
+
+
+
+//Array
+
+var array=[10,20,30,40,50];
+// console.log(array);
+// console.log(array.length);
+
+// console.log(array[2]);
+
+
+var Mofij =["Sadia","Nadia"]
+
+Mofij.push("Rahima");
+Mofij.push("Karina");
+Mofij.push("Anushka");
+Mofij.push("Aliya");
+console.log(Mofij);
+Mofij.pop();
+Mofij.pop();
+Mofij.pop();
+// console.log(Mofij);
